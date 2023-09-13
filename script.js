4c0fb42945fa9bfd842375268c8d0d3debdac825
Adarsh Gupta <114940577+adarshg3371@users.noreply.github.com>
2023-09-13 11:45:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-
+const lookup = {
+  A: "N",
+  B: "O",
+  C: "P",
+  D: "Q",
+  E: "R",
+  F: "S",
+  G: "T",
+  H: "U",
+  I: "V",
+  J: "W",
+  K: "X",
+  L: "Y",
+  M: "Z",
+  N: "A",
+  O: "B",
+  P: "C",
+  Q: "D",
+  R: "E",
+  S: "F",
+  T: "G",
+  U: "H",
+  V: "I",
+  W: "J",
+  X: "K",
+  Y: "L",
+  Z: "M",
+  "?": "?",
+  ",": ",",
+};
+ 
+function rot13(encodedStr) {
+  let decodedArr = []; // Your Result goes here
+  // Only change code below this line
+	for(let i=0; i<str.lenght:i++){
+		if(lookup[str[i]]==undefined){
+			decodedArr.push(str[i])
+ 
+		else decodedArr.push(lookup[str[i]])
+	}
+ 
+  return; 
+}
